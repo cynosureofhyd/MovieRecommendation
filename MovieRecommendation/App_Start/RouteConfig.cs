e5d3eacd782d09f84358a364d12ef989d9725f50
cynosureofhyd2 <tomu646@gmail.com>
2013-11-24 10:31:32 -0700

    Initial way for loading data into db

--- MovieRecommendation/App_Start/RouteConfig.cs
+++ MovieRecommendation/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MovieRecommendation
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Movies", action = "Get", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
--- MovieRecommendation/Controllers/MoviesController.cs
+++ MovieRecommendation/Controllers/MoviesController.cs
@@ -19,15 +19,86 @@ namespace MovieRecommendation.Controllers
     {
         public ActionResult Get()
         {
+            //AddSampleRoleData();
             string inputpath = "C:\\Users\\PrashMaya\\Desktop\\IMDBMovieTitleIds-0-2500.txt";
             string inputfolder = "C:\\Users\\PrashMaya\\My Documents\\First2500MoviesIMDB\\Movie{0}.txt";
-
             inputpath = String.Format(inputfolder, 1);
-            string text = System.IO.File.ReadAllText(@inputpath);
-            dynamic obj = ConvertToObj(text);
+
+            for(int i = 0; i < 2451; i++)
+            {
+                inputpath = String.Format(inputfolder, i);
+                string text = System.IO.File.ReadAllText(@inputpath);
+                dynamic obj = ConvertToObj(text);
+                LoadDataIntoDb(obj);
+            }
+            
             HashSet<string> keys = new HashSet<string>();
             string[] lines = System.IO.File.ReadAllLines(@inputpath);
             return View();
+        }
+
+        private static void LoadDataIntoDb(dynamic obj)
+        {
+            try
+            {
+                MovieEntities db = new MovieEntities();
+               
+                string imdbId = obj[0]["imdb_id"];
+                
+                //if(!db.Movies.Select(m => m.ImdbID == imdbId).First())
+                if(db.Movies.Count() == 0 || !db.Movies.Select(m => m.ImdbID == imdbId).First())
+                {
+                    Int64 existingMovieId = 0;
+                    if (db.Movies.Count() > 0)
+                        existingMovieId = db.Movies.Count();
+                    Movie movie = new Movie();
+                    movie.ID = existingMovieId + 1;
+                    movie.PlotDetailed = obj[0]["plot"];
+                    movie.ImdbID = obj[0]["imdb_id"];
+                    movie.PlotSimple = obj[0]["plot_simple"];
+                    var tempruntime = obj[0]["runtime"];
+                    movie.Runtime = ConvertRuntime(tempruntime.ToString());
+                    movie.Rated = obj[0]["rated"];
+                    movie.IMDBRating = obj[0]["rating"];
+                    Int64 releaseDate = obj[0]["release_date"];
+                    DateTime dtTime = new DateTime(Int32.Parse(releaseDate.ToString().Substring(0, 4)), Int32.Parse(releaseDate.ToString().Substring(4, 2)), Int32.Parse(releaseDate.ToString().Substring(6, 2)));
+                    movie.ReleaseDate = dtTime;
+                    movie.Title = obj[0]["title"];
+                    Poster poster = new Poster();
+                    poster.imdb = obj[0]["poster"]["imdb"];
+                    poster.cover = obj[0]["poster"]["cover"];
+                    db.Movies.Add(movie);
+                    db.SaveChanges();
+                    var savedMovie = db.Movies.Where(m => m.ImdbID == movie.ImdbID).Distinct();
+                    PosterInfo posterInfo = new PosterInfo();
+                    posterInfo.Imdb = poster.imdb;
+                    posterInfo.Cover = poster.cover;
+                    posterInfo.MovieId = savedMovie.First().ID;
+                    posterInfo.Id = existingMovieId +1;
+                    db.PosterInfoes.Add(posterInfo);
+                    db.SaveChanges();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static int ConvertRuntime(string runtime)
+        {
+            string result = "";
+            for(int i = 0; i < runtime.Length; i++)
+            {
+                if (char.IsDigit(runtime[i]))
+                    result += runtime[i];
+            }
+            return Int32.Parse(result); ;
+        }
+
+        private static void ProcessDynamic(dynamic obj)
+        {
+
         }
 
         public static dynamic ConvertToObj(string str)
@@ -53,6 +124,16 @@ namespace MovieRecommendation.Controllers
                 dynamic moreInfo = JsonConvert.DeserializeObject(obj.ToString());
                 string temp = moreInfo[0]["plot"];
             }
+        }
+
+
+        private static void AddSampleRoleData()
+        {
+            Role role = new Role();
+            role.Description = "Director";
+            MovieEntities db = new MovieEntities();
+            db.Roles.Add(role);
+            db.SaveChanges();
         }
     }
 }
